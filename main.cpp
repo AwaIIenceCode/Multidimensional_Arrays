dec370f41b2789c8e974110d64cf637c725cf05a
Oleh Skrypnyk-Zakrevskiy <awallencecode@gmail.com>
2024-12-09 20:39:43 +0200

    ex_2 is done

--- main.cpp
+++ main.cpp
@@ -1,3 +1,8 @@
+//Напишіть програму, яка створює двовимірний масив і заповнює його за таким принципом:
+// користувач вводить число (наприклад, 3); перший елемент масиву набуває значення цього числа;
+// наступний елемент масиву набуває значення цього числа + 1 (тобто 4 для нашого прикладу);
+// третій елемент масиву — попередній елемент + 1 (тобто 5 для нашого прикладу). Створений масив вивести на екран.
+
 #include <iostream>
 #include <iomanip>
 
@@ -5,8 +10,8 @@ using namespace std;
 
 int main()
 {
-    const int RAWS = 3;
-    const int COLS = 3;
+    const int RAWS = 9;
+    const int COLS = 9;
     int arr[RAWS][COLS];
 
     int user_number;
@@ -24,14 +29,14 @@ int main()
 
             else if (j == 0)
             {
-                arr[i][j] = arr[i - 1][COLS - 1] * 2;
+                arr[i][j] = arr[i - 1][COLS - 1] + 1;
             }
 
             else
             {
-                arr[i][j] = arr[i][j - 1] * 2;
+                arr[i][j] = arr[i][j - 1] + 1;
             }
-            cout << setfill(' ') << setw(4) << arr[i][j];
+            cout << setfill(' ') << setw(3) << arr[i][j];
         }
         cout << endl;
     }
